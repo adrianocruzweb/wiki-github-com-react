557de870c5e7640ef47ca33e91fb873c6ecb6fda
adrianocruzweb <adrianocruzweb@hotmail.com>
2024-10-02 19:27:04 -0300

    componet input and component repo list

---
+++ src/components/ItemRepo/index.js
@@ -0,0 +1,14 @@
+import { ItemContainer } from "./styles";
+
+
+const ItemRepo = () => {
+  return (
+    <ItemContainer>
+      <h3>Nome repo</h3>
+      <p>Descrição repo</p>
+      <hr/>
+    </ItemContainer>
+  )
+}
+
+export default ItemRepo;
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,15 @@
-/* import './App.css'; */
+import gitLogo from '../assets/github2.png';
+import Input from '../components/Input';
+import ItemRepo from '../components/ItemRepo';
+import {Container} from './styles'
 
 function App() {
   return (
-    <div className="App">
-      OI
-    </div>
+    <Container>
+      <img src={gitLogo} width="72" height="72" alt="logo github" />
+      <Input/>
+      <ItemRepo />
+    </Container>
   );
 }
 
